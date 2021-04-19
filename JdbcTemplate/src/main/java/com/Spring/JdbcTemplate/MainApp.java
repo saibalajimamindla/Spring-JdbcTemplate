269802a0bd0feb949516ad0d171f1c99664e65d6
saiba <saiba@balaji>
2021-04-19 12:22:30 +0530

    19/04/2021

--- JdbcTemplate/src/main/java/com/Spring/JdbcTemplate/MainApp.java
+++ JdbcTemplate/src/main/java/com/Spring/JdbcTemplate/MainApp.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test {
+public class MainApp {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
